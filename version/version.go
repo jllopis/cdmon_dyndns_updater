34543d860ff2338c10f67f5f38a4f9edf1c0c68f
Joan Llopis <jllopisg@gmail.com>
2020-12-25 14:13:59 +0100

    Added version package

---
+++ version/version.go
@@ -0,0 +1,40 @@
+package version
+
+import "fmt"
+
+const (
+	// Name of the service
+	Name = "CorpusMicroServer"
+	// APIVersion of the API this service provide
+	APIVersion = "v1"
+)
+
+var (
+	// Version is the current echo version in SemVer format
+	SemVer = "v0.0.1"
+	// BuildDate represents the date this service was built
+	BuildDate string
+	// GitCommit is the hash of the git commit
+	GitCommit string
+)
+
+type Version struct {
+	APIVersion string `json:"api_version"`
+	Version    string `json:"version"`
+	GitCommit  string `json:"git_commit"`
+	BuildDate  string `json:"build_date"`
+}
+
+func Get() *Version {
+	v := &Version{
+		APIVersion: APIVersion,
+		Version:    SemVer,
+		GitCommit:  GitCommit,
+		BuildDate:  BuildDate,
+	}
+	return v
+}
+
+func (v *Version) String() string {
+	return fmt.Sprintf("%s (%s) %s", SemVer, GitCommit, BuildDate)
+}
